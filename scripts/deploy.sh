a80f7fdd78e4409755b038bf973f7ad0d157e7e4
Nathan Toups <n@rjrbt.io>
2019-03-20 15:58:23 -0600

    updated deploy script to make life easier

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,13 +3,12 @@
 # exit on error
 set -e
 
-DIR=site
-BUCKET=s3://nomasters.io
-DIST_ID=$NOMASTERS_CDN_DIST_ID
+DIR=src
+BUCKET=s3://n.2p5.xyz
+DIST_ID=$N_2P5_XYZ_CDN_DIST_ID
 CACHE_ID=`date +%s`
 FORMATTED_JSON='{"Paths": {"Quantity": 1,"Items": ["/*"]},"CallerReference":"'${CACHE_ID}'"}'
 
 cd $DIR
-hugo
-aws s3 sync public/ $BUCKET --acl public-read
+aws s3 sync . $BUCKET --acl public-read
 aws cloudfront create-invalidation --distribution-id $DIST_ID --invalidation-batch "$FORMATTED_JSON"
