ab086ecc82b0ee6ba3f85b28f9695bf206b631c2
Nathan Toups <n@rjrbt.io>
2020-11-19 22:10:46 -0700

    doc: added Peak Cyber to speaking list, updated index to more closely reflect reality

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,9 +6,9 @@ set -e
 DIR=src
 BUCKET=s3://n.2p5.xyz
 DIST_ID=$N_2P5_XYZ_CDN_DIST_ID
-CACHE_ID=`date +%s`
+CACHE_ID=$(date +%s)
 FORMATTED_JSON='{"Paths": {"Quantity": 1,"Items": ["/*"]},"CallerReference":"'${CACHE_ID}'"}'
 
 cd $DIR
 aws s3 sync . $BUCKET --acl public-read
-aws cloudfront create-invalidation --distribution-id $DIST_ID --invalidation-batch "$FORMATTED_JSON"
+aws cloudfront create-invalidation --distribution-id "$DIST_ID" --invalidation-batch "$FORMATTED_JSON"
