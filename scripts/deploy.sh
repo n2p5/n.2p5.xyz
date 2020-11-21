d12351c9d66e456308a4c26ba61baeacdce7c268
Nathan Toups <n@rjrbt.io>
2020-11-20 21:13:29 -0700

    fix: shellcheck linting

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ BUCKET=s3://n.2p5.xyz
 DIST_ID=$N_2P5_XYZ_CDN_DIST_ID
 CACHE_ID=$(date +%s)
 FORMATTED_JSON='{"Paths": {"Quantity": 1,"Items": ["/*"]},"CallerReference":"'${CACHE_ID}'"}'
-
+w
 cd $DIR
 aws s3 sync . $BUCKET --acl public-read
 aws cloudfront create-invalidation --distribution-id "$DIST_ID" --invalidation-batch "$FORMATTED_JSON"
